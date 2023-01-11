19d6b1590f70c5f4215c178515b8d062f6f657c2
dev-michel324 <dev_michek324@protonmail.com>
2023-01-10 21:16:00 -0300

    added data to show in task list

---
+++ src/components/task/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        id: 1,
+        description: "Sleep",
+        status: "open"
+    },
+    {
+        id: 2,
+        description: "Eat",
+        status: "closed"
+    },
+    {
+        id: 3,
+        description: "Code",
+        status: "open"
+    }
+];
