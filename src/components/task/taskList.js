7a9daeba1db4966b76213b0ae9007374bccd9fec
dev-michel324 <dev_michek324@protonmail.com>
2023-01-13 14:42:32 -0300

    feat: task is from api now

--- src/components/task/taskList.js
+++ src/components/task/taskList.js
@@ -1,14 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Task from "./task";
-
-import { data as tasksData } from "./data";
+import { Api } from "../../services/api";
 
 const TaskList = () => {
     const [tasks, setTasks] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    const getAllTasks = () => {
+        Api.get("todo").then((res) => {
+            if(res.status >= 200 && res.status <= 299){
+                setTasks(res.data.todos);
+                setIsLoading(false);
+            }
+            else{
+                setIsError(true);
+                setIsLoading(false);
+                throw new Error(res);
+            }
+        }).catch((err) => {
+            setIsError(true);
+            setIsLoading(false);
+            console.log(err);
+        })
+    };
 
     useEffect(() => {
-        setTasks(tasksData);
+        getAllTasks();
+        console.log(tasks);
     }, []);
+
+    if(isLoading)
+        return <h1>Loading tasks...</h1>
+    if(isError)
+        return <h1 style={{color: "red"}}>Error on get tasks...</h1>
 
     return <section className="task_list">
         {tasks.map(task => {
