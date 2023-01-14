eb0ef65001430e982a7ca7bc0e5fe849f0c17cf8
dev-michel324 <dev_michek324@protonmail.com>
2023-01-14 13:16:16 -0300

    feat: if dont have tasks, so render a message

--- src/components/task/taskList.js
+++ src/components/task/taskList.js
@@ -3,43 +3,44 @@ import Task from "./task";
 import { Api } from "../../services/api";
 
 const TaskList = () => {
-    const [tasks, setTasks] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-    const getAllTasks = () => {
-        Api.get("todo").then((res) => {
-            if(res.status >= 200 && res.status <= 299){
-                setTasks(res.data.todos);
-                setIsLoading(false);
-            }
-            else{
-                setIsError(true);
-                setIsLoading(false);
-                throw new Error(res);
-            }
-        }).catch((err) => {
-            setIsError(true);
-            setIsLoading(false);
-            console.log(err);
-        })
-    };
+  const getAllTasks = () => {
+    Api.get("todo")
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          setTasks(res.data.todos);
+          setIsLoading(false);
+        } else {
+          setIsError(true);
+          setIsLoading(false);
+          throw new Error(res);
+        }
+      })
+      .catch((err) => {
+        setIsError(true);
+        setIsLoading(false);
+        console.log(err);
+      });
+  };
 
-    useEffect(() => {
-        getAllTasks();
-        console.log(tasks);
-    }, []);
+  useEffect(() => {
+    getAllTasks();
+  }, []);
 
-    if(isLoading)
-        return <h1>Loading tasks...</h1>
-    if(isError)
-        return <h1 style={{color: "red"}}>Error on get tasks...</h1>
+  if (isLoading) return <h1>Loading tasks...</h1>;
+  if (isError) return <h1 style={{ color: "red" }}>Error on get tasks...</h1>;
+  if (tasks.length === 0) return <h1 style={{color: "gray"}}>You don't have tasks!</h1>;
 
-    return <section className="task_list">
-        {tasks.map(task => {
-            return <Task key={task.id} {...task} />
-        })}
+  return (
+    <section className="task_list">
+      {tasks.map((task) => {
+        return <Task key={task.id} {...task} />;
+      })}
     </section>
-}
+  );
+};
 
 export default TaskList;
